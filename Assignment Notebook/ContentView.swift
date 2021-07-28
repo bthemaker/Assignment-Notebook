840490c97e47e51589c1e6e7f4dc649a7856605a
Bruno Garcia <bthemaker@gmail.com>
2021-07-28 13:46:30 -0400

    Added Functional date thingy

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var assignments = ["Assignment 1","Assignment 2","Assignment 3","Assignment 4",]
+    @State private var assignments =
+        [AssignmentItem(description: "Math Homework", course: "Math", dueDate: Date()),
+         AssignmentItem(description: "Science Homework", course: "Science", dueDate: Date()),
+         AssignmentItem(description: "History Homework", course: "History", dueDate: Date())]
+    
     var body: some View {
         NavigationView {
             List {
-                ForEach(assignments, id: \.self) { assignment in
-                    Text(assignment)
+                ForEach(assignments) { assignment in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(assignment.description)
+                                .font(.headline)
+                            Text(assignment.course)
+                        }
+                        Spacer()
+                        Text(assignment.dueDate, style: .date)
+                    }
+                    
                 }
                 .onMove(perform: { indices, newOffset in assignments.move(fromOffsets: indices, toOffset: newOffset)
                 })
@@ -32,4 +45,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct AssignmentItem: Identifiable {
+    var id = UUID()
+    var description = String()
+    var course = String()
+    var dueDate = Date()
 }
